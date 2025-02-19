7f4fff988ede753bc0c2ca2a496350602ce55b51
Max Edwards <youwontforgetthis@gmail.com>
2025-02-19 16:40:01 -0500

    Added percentage through document to status line

--- init.lua
+++ init.lua
@@ -784,7 +784,7 @@ require('lazy').setup({
       -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
-        return '%2l:%-2v'
+        return '%p%% %2l:%-2v'
       end
 
       -- ... and there is more!
